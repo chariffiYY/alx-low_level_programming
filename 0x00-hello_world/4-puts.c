7242098d6a500e4fb67d24f20c4d82e772762870
chariffiYY <chariffifatimaezzahrae@gmail.com>
2023-07-16 05:47:57 -0700

    Hello-size

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #!/bin/bash
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - prints "Programming is like building a multilingual puzzle"
- * Return: 0
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
