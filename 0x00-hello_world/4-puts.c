98319e66ff8120aaf3e554546176cd80beb4e629
chariffiYY <chariffifatimaezzahrae@gmail.com>
2023-07-16 04:52:56 -0700

    Hello-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
 /**
- * main - main block
+ * main - prints "Programming is like building a multilingual puzzle"
  * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
