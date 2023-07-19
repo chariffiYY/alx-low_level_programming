494b12283670c97240cde45c8619047b4aa23653
chariffiYY <chariffifatimaezzahrae@gmail.com>
2023-07-19 11:39:20 -0700

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_alphabet - Prints the alphabet followed by a new line.
+*/
+void print_alphabet(void)
+{
+int input;
+
+for (input = 'a'; input <= 'z'; ++input)
+_putchar(input);
+_putchar('\n');
+}
+
